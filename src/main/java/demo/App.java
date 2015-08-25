f6241b77c20e6c9c8b5d16e0e0243d7a609a5458
Stephane Nicoll <snicoll@pivotal.io>
2015-08-25 11:40:39 +0200

    Basic cache demonstration
    If we enable `org.hibernate.SQL` we will see that we hit the database
    every time we need to retrieve a book by id. There are multiple places
    where we could add caching but for the sake of the demo we're going to
    add some basic cache instructions on the repository itself.
    
    First, let's add the cache starter and add `@EnableCaching` on our Spring
    Boot application. With Spring Boot 1.3, this is all that's needed to
    configure the caching infrastructure.
    
    Then we add a `@Cacheable` on `findOne` and a `@CacheEvict` on
    `delete`. Again, more cache semantics should be added to make sure you
    don't return staled data.
    
    If we run our app again, we can see that the SQL is only issued once per
    book. We can also hit the `/metrics` endpoint to show the cache metrics
    that Spring Boot exposes out-of-the-box. Since we haven't specified
    anything, Spring Boot is using a basic concurrent hash map
    implementation.

--- src/main/java/demo/App.java
+++ src/main/java/demo/App.java
@@ -6,12 +6,14 @@ import org.slf4j.LoggerFactory;
 import org.springframework.amqp.core.Queue;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.event.EventListener;
 import org.springframework.data.rest.core.event.AfterCreateEvent;
 import org.springframework.stereotype.Service;
 
 @SpringBootApplication
+@EnableCaching
 public class App {
 
 	public static void main(String[] args) {
--- src/main/java/demo/domain/BookRepository.java
+++ src/main/java/demo/domain/BookRepository.java
@@ -2,11 +2,23 @@ package demo.domain;
 
 import java.util.Collection;
 
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RestResource;
 
+@CacheConfig(cacheNames = "books")
 public interface BookRepository extends CrudRepository<Book, Long> {
+
+	@Override
+	@Cacheable
+	Book findOne(Long aLong);
+
+	@CacheEvict
+	@Override
+	void delete(Long aLong);
 
 	@RestResource(path = "by-isbn")
 	Book findByIsbn(@Param("id") String isbn);
