dd99ca7b876310ed024bdcdbcc380bb469338dd0
Stephane Nicoll <snicoll@pivotal.io>
2015-08-25 10:03:18 +0200

    EventListener demonstration
    Spring 4.2 makes it very easy to declare an event listener. Just add the
    `@EventListener` annotation on a method of a bean with the type of event
    you are listening to.
    
    Spring Data REST is actually sending various events related to
    repositories actions. We add a listener for `AfterCreatedEvent` that only
    prints the entity that was just created.
    
    We can post the json representation of a book to `/data/books` to see
    what happens.

--- src/main/java/demo/App.java
+++ src/main/java/demo/App.java
@@ -1,7 +1,13 @@
 package demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.event.EventListener;
+import org.springframework.data.rest.core.event.AfterCreateEvent;
+import org.springframework.stereotype.Service;
 
 @SpringBootApplication
 public class App {
@@ -9,4 +15,17 @@ public class App {
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
 	}
+
+	@Service
+	static class EntityCreatedHandler {
+
+		private static final Logger logger = LoggerFactory.getLogger(EntityCreatedHandler.class);
+
+		@EventListener
+		public void handleEntityCreated(AfterCreateEvent event) {
+			logger.info("Created " + event.getSource());
+		}
+
+	}
+
 }
